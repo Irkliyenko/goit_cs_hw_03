b8997280257327e848a8809b1eb97e9bad8e297a
Irkliyenko <irkliyenko123@gmail.com>
2024-03-11 11:23:39 -0700

    Postgres reqs

--- PostgresSQL/requests.sql
+++ PostgresSQL/requests.sql
@@ -2,10 +2,17 @@
 SELECT title FROM task WHERE user_id = 7;
 
 -- Select tasks by a specific status.
-SELECT title FROM task WHERE status_id = 1;
+SELECT t.title, s.name
+FROM task t
+JOIN status s ON t.status_id = s.id
+WHERE s.name = 'new';
+
 
 -- Update the status of a specific task.
-UPDATE task SET status_id = 3 WHERE status_id = 2;
+UPDATE tasks
+SET status_id = (SELECT id FROM status WHERE name = 'in progress')
+WHERE id = 1;
+
 
 -- Get a list of users who have no tasks.
 SELECT fullname FROM users WHERE id NOT IN (SELECT user_id FROM task);
@@ -15,7 +22,10 @@ INSERT INTO task (title, description, status_id, user_id)
 VALUES ('work hard', 'work hard means no lunch and 60h work week', 1, 3);
 
 -- Get all tasks that are not yet completed.
-SELECT title FROM task WHERE status_id <> 3;
+SELECT t.title, s.name
+FROM task t
+JOIN status s ON t.status_id = s.id
+WHERE s.name <> 'Completed';
 
 -- Delete a specific task. Use DELETE to remove a task by its id.
 DELETE FROM task WHERE id = 3;
@@ -27,7 +37,10 @@ SELECT * FROM users WHERE email LIKE '%52@%';
 UPDATE users SET fullname = 'Alex String' WHERE id = 1;
 
 -- Get the number of tasks for each status. Use SELECT, COUNT, GROUP BY to group tasks by statuses.
-SELECT status_id, COUNT(title) FROM task GROUP BY status_id;
+SELECT s.name AS status_name, COUNT(t.id) AS task_count
+FROM task t
+JOIN status s ON t.status_id = s.id
+GROUP BY s.name;
 
 -- Get tasks assigned to users with a specific domain part of the email.
 SELECT t.title, u.fullname
@@ -39,10 +52,11 @@ WHERE u.email LIKE '%@example.com';
 SELECT * FROM task WHERE description IS NULL;
 
 -- Select users and their tasks that are in 'In Progress' status.
-SELECT u.fullname, t.title
+SELECT u.fullname, t.title, s.name 
 FROM users u
-JOIN task t ON u.id = t.user_id
-WHERE t.status_id = 2;
+INNER JOIN task t ON u.id = t.user_id
+INNER JOIN status s ON t.status_id = s.id
+WHERE s.name = 'In Progress';
 
 -- Get users and the number of their tasks. Use LEFT JOIN and GROUP BY to select users and count their tasks.
 SELECT u.fullname, COUNT(t.title)
